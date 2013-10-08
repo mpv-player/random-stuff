83254823c6337937c0e9da07aedd509c2d81b070
Ben Boeckel <mathstuf@gmail.com>
2013-10-07 23:38:35 -0400

    matroska: create base test videos

--- matroska/chapters/mkvideos.sh
+++ matroska/chapters/mkvideos.sh
@@ -5,8 +5,9 @@ image_format="png"
 size="100x100"
 
 # Video settings
-fps="30"
 seconds="2"
+fps="20"
+codec="libtheora"
 
 create_image () {
     color="$1"
@@ -23,17 +24,30 @@ create_list () {
     printf "%${count}s" "" | sed -e "s/ /$color.$image_format\n/g" > "$color.txt"
 }
 
+create_video () {
+    color="$1"
+    shift
+
+    mpv -v "mf://@$color.txt" -o "$color.mkv" \
+        -fps "$fps" \
+        -ofps "$fps" \
+        -vo null \
+        -vf scale \
+        -ovc "$codec"
+}
+
 for_each_color () {
     cmd="$1"
     shift
 
     "$cmd" red "$@"
     "$cmd" blue "$@"
-    "$cmd" green "$@"
     "$cmd" yellow "$@"
     "$cmd" white "$@"
     "$cmd" black "$@"
 }
 
+# Create small videos of each color.
 for_each_color create_image
 for_each_color create_list
+for_each_color create_video
